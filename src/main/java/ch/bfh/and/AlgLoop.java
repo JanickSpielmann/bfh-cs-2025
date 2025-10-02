ce3cc67515ede483c00173c04a780cff41c61b5f
Janick <janick.spielmann@messageme.ch>
2025-10-02 13:23:59 +0200

    fix and clean up algorithm and datastructures

--- src/main/java/ch/bfh/and/AlgLoop.java
+++ src/main/java/ch/bfh/and/AlgLoop.java
@@ -2,34 +2,34 @@ package ch.bfh.and;
 
 public class AlgLoop {
 
-    static void main() {
 
-        double t =  System.currentTimeMillis();
-        task11(80);
+    public static void main(String[] args) {
 
-        System.out.println((System.currentTimeMillis()-t)/1000);
+        double t = System.currentTimeMillis();
+        task11(20);
+
+        System.out.println((System.currentTimeMillis() - t) / 1000);
     }
 
-    static void task11(double i) {
+    static void task11(int i) {
 
-       int s = 0;
-       for (int j=1;j<Math.pow(i,2);j++) {
-           s += j;
+        int s = 0;
+        for (int j = 1; j < i * i; j++) {
+            s += j;
 
-           if(s%2 != 0){
-               for (int k=j;k<Math.pow(j,2);k++) {
-                   s -= k;
-               }
-               System.out.println("loadingOdd");
-           }
-           else{
-               for (int k=j;k<j+100;k++) {
-                   s += k;
-               }
-               System.out.println("loadingEven");
-           }
+            if (s % 2 != 0) {
+                for (int k = j; k < j * j; k++) {
+                    s -= k;
+                }
+                System.out.println("loadingOdd");
+            } else {
+                for (int k = j; k < (j + 100); k++) {
+                    s += k;
+                }
+                System.out.println("loadingEven");
+            }
 
-       }
+        }
 
     }
 }
